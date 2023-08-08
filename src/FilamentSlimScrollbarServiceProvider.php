b67b712afde3fb1e2dba52f8aa784e7f5237ecf8
aymanalhattami <ayman.m.alhattami@gmail.com>
2023-08-08 11:21:47 +0300

    fix: include css file

--- src/FilamentSlimScrollbarServiceProvider.php
+++ src/FilamentSlimScrollbarServiceProvider.php
@@ -2,7 +2,9 @@
 
 namespace Aymanalhattami\FilamentSlimScrollbar;
 
+use Filament\Support\Assets\Css;
 use Spatie\LaravelPackageTools\Package;
+use Filament\Support\Facades\FilamentAsset;
 use Spatie\LaravelPackageTools\PackageServiceProvider;
 
 
@@ -10,13 +12,16 @@ class FilamentSlimScrollbarServiceProvider extends PackageServiceProvider
 {
     public static string $name = 'filament-slim-scrollbar';
 
-    protected array $styles = [
-        'filament-slim-scrollbar' => __DIR__ . '/../resources/dist/filament-slim-scrollbar.css',
-    ];
-
     public function configurePackage(Package $package): void
     {
         $package->name(static::$name)
             ->hasAssets();
+
+            FilamentAsset::register(
+                assets: [
+                    Css::make(static::$name, __DIR__ . '/../resources/dist/app.css'),
+                ],
+                package: 'aymanalhattami/filament-slim-scrollbar'
+            );
     }
 }
